cf84f50ffe5d025a4c92250c211180cb83fba574
kossick <tjbullock89@gmail.com>
2022-12-11 10:57:45 +0200

    Rebuilt day 10

--- day_10/day_10.py
+++ day_10/day_10.py
@@ -1,7 +1,7 @@
 import sys
 import pathlib
 import argparse
-from typing import List
+from typing import List, Tuple
 
 CURRENT_PATH = pathlib.Path.cwd()
 
@@ -20,6 +20,38 @@ def read_file(file: pathlib.Path) -> List[str]:
     return [data.rstrip('\n') for data in source_data]
 
 
+def parse_line(line: str) -> Tuple[str, int]:
+    line_split = line.split(' ')
+    value = 0
+    if len(line_split) > 1:
+        value = int(line_split[1])
+    command = line_split[0]
+    return (command, value)
+
+
+def update_signals(
+    cpu_cycle: int,
+    register_value: int,
+    signal_strengths: List[int]
+) -> List[int]:
+    if (cpu_cycle - 20) % 40 == 0:
+        signal_strengths.append(cpu_cycle * register_value)
+    return signal_strengths
+
+
+def update_crt(
+    crt: list[str],
+    cycle: int,
+    sprite_centre: int
+) -> List[str]:
+    crt.append(
+        '#'
+        if (cycle - 1) % 40 in range(sprite_centre - 1, sprite_centre + 2)
+        else '.'
+    )
+    return crt
+
+
 def main() -> None:
     parser = generate_arg_parser()
     args = parser.parse_args()
@@ -36,6 +68,51 @@ def main() -> None:
             "check that it exists and that you spelt it correctly!"
         )
         sys.exit(1)
+    cpu_cycle = 1
+    register_value = 1
+    # Part 1
+    # signal_strengths = list()
+    # Part 2
+    crt_display = list()
+    for line in raw_data:
+        command, value = parse_line(line)
+        if command == 'addx':
+            for _ in range(2):
+                # Part 1
+                # signal_strengths = update_signals(
+                #     cpu_cycle,
+                #     register_value,
+                #     signal_strengths
+                #     )
+                # Part 2
+                crt_display = update_crt(
+                    crt_display,
+                    cpu_cycle,
+                    register_value
+                )
+                cpu_cycle += 1
+            register_value += value
+        else:
+            # Part 1
+            # signal_strengths = update_signals(
+            #         cpu_cycle,
+            #         register_value,
+            #         signal_strengths
+            #         )
+            # Part 2
+            crt_display = update_crt(
+                crt_display,
+                cpu_cycle,
+                register_value
+            )
+            cpu_cycle += 1
+    # Part 1
+    # print(f"Total sum of signal strengths is {sum(signal_strengths)}")
+    # Part 2
+    rendered_image = [
+        ''.join(crt_display[n * 40: (n + 1) * 40]) for n in range(6)
+    ]
+    print(*rendered_image, sep='\n')
 
 
 if __name__ == "__main__":
