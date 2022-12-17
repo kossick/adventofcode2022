00bc464922436e760ee0d96b8eb568a00ff048de
kossick <tjbullock89@gmail.com>
2022-12-17 09:58:40 +0200

    Added day 13

--- day_13/day_13.py
+++ day_13/day_13.py
@@ -1,7 +1,10 @@
+from ast import literal_eval
+from functools import cmp_to_key
+from math import prod
 import sys
 import pathlib
 import argparse
-from typing import List
+from typing import List, Tuple
 
 CURRENT_PATH = pathlib.Path.cwd()
 
@@ -20,6 +23,52 @@ def read_file(file: pathlib.Path) -> List[str]:
     return [data.rstrip('\n') for data in source_data]
 
 
+def split_into_pairs(data: List[str]) -> List[Tuple[str, str]]:
+    pairs = list()
+    pair = tuple()
+    for line in data:
+        if line == '':
+            pairs.append(pair)
+            pair = tuple()
+            continue
+        pair += (line,)
+    pairs.append(pair)
+    return pairs
+
+
+def convert_strings(pairs: List[Tuple[str, str]]) -> List[Tuple[List, List]]:
+    messages = list()
+    for pair in pairs:
+        message_pair = tuple()
+        for string in pair:
+            message_pair += (literal_eval(string),)
+        messages.append(message_pair)
+    return messages
+
+
+def is_in_correct_order(
+    left: List[int | list],
+    right: List[int | list]
+) -> int:
+    left = [left] if isinstance(left, int) else left
+    right = [right] if isinstance(right, int) else right
+    for left_elem, right_elem in zip(left, right):
+        if isinstance(left_elem, list) or isinstance(right_elem, list):
+            result = is_in_correct_order(left_elem, right_elem)
+        else:
+            result = right_elem - left_elem
+        if result != 0:
+            return result
+    return len(right) - len(left)
+
+
+def find_correctly_ordered(packets: List[Tuple[List, List]]) -> List[int]:
+    return [
+        index for index, pair in enumerate(packets, 1)
+        if is_in_correct_order(*pair) > 0
+    ]
+
+
 def main() -> None:
     parser = generate_arg_parser()
     args = parser.parse_args()
@@ -36,6 +85,30 @@ def main() -> None:
             "check that it exists and that you spelt it correctly!"
         )
         sys.exit(1)
+    # Part 1
+    string_pairs = split_into_pairs(raw_data)
+    message_pairs = convert_strings(string_pairs)
+    correctly_ordered_pairs = find_correctly_ordered(message_pairs)
+    print(
+        "The sum of the indices in the correct order "
+        f"is {sum(correctly_ordered_pairs)}"
+    )
+    # Part 2
+    packets = [
+        literal_eval(line) for line in raw_data if line != ''
+    ] + [[[2]], [[6]]]
+    sorted_packets = sorted(
+        packets,
+        key=cmp_to_key(is_in_correct_order),
+        reverse=True
+    )
+    decoder_indices = [
+        n for n, packet in enumerate(sorted_packets, 1)
+        if packet in ([[2]], [[6]])
+    ]
+    print(
+        f"The decoder key is {prod(decoder_indices)}"
+    )
 
 
 if __name__ == "__main__":
